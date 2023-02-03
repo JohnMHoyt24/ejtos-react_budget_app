ab4b8c10e0818da4d94d601f98f9b079bd4d13f7
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2023-02-03 15:35:10 -0500

    Created the components

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,16 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+
+    return(
+        /*This componenet provide the structure and style of the
+        budget component on the upper-left of the app. */
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
