fb968d441d602a250a1672a2b8f19ac9229dbc44
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2023-02-06 16:19:45 -0500

    Updated App.js and Currency.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
     const [budget, setBudget] = useState('');
     const { expenses } = useContext(AppContext);
